6d6ec13e40fa8fdb8449db233eda3765a22d2b1c
Denis Osyushkin <denisitch@gmail.com>
2024-04-28 20:44:32 +0300

    add catalogue-service module

---
+++ catalogue-service/src/main/java/com/denisitch/catalogue/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.denisitch.catalogue.repository;
+
+import com.denisitch.catalogue.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
