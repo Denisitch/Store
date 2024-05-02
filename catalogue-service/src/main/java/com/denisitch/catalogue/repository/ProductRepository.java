dac3b4355ef74ca61e8c503ade671a918f1bfaef
Denis Osyushkin <denisitch@gmail.com>
2024-05-02 22:17:49 +0300

    add custom named query

--- catalogue-service/src/main/java/com/denisitch/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/com/denisitch/catalogue/repository/ProductRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 //    @Query(value = "select p from Product p where p.title ilike :filter")
-    @Query(value = "select * from catalogue.t_product where c_title ilike :filter", nativeQuery = true)
+//    @Query(value = "select * from catalogue.t_product where c_title ilike :filter", nativeQuery = true)
+    @Query(name = "Product.findAllByTitleLikeIgnoringCase", nativeQuery = true)
     Iterable<Product> findByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
