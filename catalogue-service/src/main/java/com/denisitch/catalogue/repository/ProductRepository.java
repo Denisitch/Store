b74337bdb41ea9498f2f38927eebfb1128158361
Denis Osyushkin <denisitch@gmail.com>
2024-05-02 22:06:58 +0300

    add custom query with jpql

--- catalogue-service/src/main/java/com/denisitch/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/com/denisitch/catalogue/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.denisitch.catalogue.repository;
 
 import com.denisitch.catalogue.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Iterable<Product> findByTitleLikeIgnoreCase(String title);
+    @Query(value = "select p from Product p where p.title ilike :filter")
+    Iterable<Product> findByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
