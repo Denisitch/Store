c9463f9fcc7f24af80785c185c63ffcb2d47c627
Denisitch <denisitch@gmail.com>
2024-05-07 13:30:03 +0300

    add integration test for get new product page

--- manager-app/src/test/java/com/denisitch/manager/controller/ProductsControllerIT.java
+++ manager-app/src/test/java/com/denisitch/manager/controller/ProductsControllerIT.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,5 +20,17 @@ public class ProductsControllerIT {
     MockMvc mockMvc;
 
     @Test
-    void getNewProductPage_ReturnsProductPage() throws Exception {}
+    void getNewProductPage_ReturnsProductPage() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.get("/catalogue/products/create")
+                .with(user("j.dewar").roles("MANAGER"));
+        // when
+        this.mockMvc.perform(requestBuilder)
+        //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("catalogue/products/new_product")
+                );
+    }
 }
