a65548ea487ca0881b0456ee744844dff6f4ef56
Denis Osyushkin <denisitch@gmail.com>
2024-05-10 19:35:52 +0300

    add integration tests for create product

--- catalogue-service/src/test/java/com/denisitch/catalogue/controller/ProductsRestControllerIT.java
+++ catalogue-service/src/test/java/com/denisitch/catalogue/controller/ProductsRestControllerIT.java
@@ -4,16 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Locale;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Transactional
 @SpringBootTest
@@ -45,4 +47,71 @@ class ProductsRestControllerIT {
                                 """)
                 );
     }
+
+    @Test
+    void createProduct_RequestIsValid_ReturnsCreatedProduct() throws Exception {
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "Еще один новый товар", "details": "Какое-то описание нового товара"}
+                        """)
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isCreated(),
+                        header().string(HttpHeaders.LOCATION, "http://localhost/catalogue-api/products/1"),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {
+                                "id": 1,
+                                "title": "Еще один новый товар",
+                                "details": "Какое-то описание нового товара"
+                                }
+                                """));
+    }
+
+    @Test
+    void createProduct_RequestIsNotValid_ReturnsProblemDetail() throws Exception {
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "  ", "details": null}
+                        """)
+                .locale(Locale.of("ru", "RU"))
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                  "errors": [
+                                    "Название товара должно быть от 3 до 50 символов"
+                                  ]
+                                }
+                                """));
+    }
+
+    @Test
+    void createProduct_UserIsNotAuthorized_ReturnsForbidden() throws Exception {
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "  ", "details": null}
+                        """)
+                .locale(Locale.of("ru", "RU"))
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        this.mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden()
+                );
+    }
+
+
 }
