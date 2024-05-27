39b19e13ef2d2d05052aba45e40d9fcf2fb06244
Denisitch <denisitch@gmail.com>
2024-05-27 15:42:56 +0300

    add favourite products rest controller

--- feedback-service/src/main/java/com/denisitch/feedback/controller/ProductReviewsRestController.java
+++ feedback-service/src/main/java/com/denisitch/feedback/controller/ProductReviewsRestController.java
@@ -5,13 +5,8 @@ import com.denisitch.feedback.entity.ProductReview;
 import com.denisitch.feedback.service.ProductReviewsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -40,16 +35,5 @@ public class ProductReviewsRestController {
                         .created(uriComponentsBuilder.replacePath("/feedback-api/product-reviews/{id}")
                                 .build(productReview.getId()))
                         .body(productReview));
-    }
-
-    @ExceptionHandler(WebExchangeBindException.class)
-    public Mono<ResponseEntity<ProblemDetail>> handleWebExchangeBindException(WebExchangeBindException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        problemDetail.setProperty("errors", ex.getAllErrors().stream()
-                .map(ObjectError::getDefaultMessage)
-                .toList());
-        return Mono.just(ResponseEntity.badRequest()
-                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
-                .body(problemDetail));
     }
 }
