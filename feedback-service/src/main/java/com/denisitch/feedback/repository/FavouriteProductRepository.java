86bd1c9c2632e76a2ef75bb685d20ce720d42066
Denisitch <denisitch@gmail.com>
2024-05-29 14:34:25 +0300

    add info about user to favourite product

--- feedback-service/src/main/java/com/denisitch/feedback/repository/FavouriteProductRepository.java
+++ feedback-service/src/main/java/com/denisitch/feedback/repository/FavouriteProductRepository.java
@@ -2,13 +2,16 @@ package com.denisitch.feedback.repository;
 
 import com.denisitch.feedback.entity.FavouriteProduct;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface FavouriteProductRepository extends ReactiveCrudRepository<FavouriteProduct, UUID> {
 
-    Mono<Void> deleteByProductId(int productId);
+    Flux<FavouriteProduct> findAllByUserId(String userId);
 
-    Mono<FavouriteProduct> findByProductId(int productId);
+    Mono<FavouriteProduct> findByProductIdAndUserId(int productId, String userId);
+
+    Mono<Void> deleteByProductIdAndUserId(int productId, String userId);
 }
