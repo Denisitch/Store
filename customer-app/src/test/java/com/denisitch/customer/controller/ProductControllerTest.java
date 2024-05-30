6258773bf989405f6204f68cc262951d7fdd9806
Denisitch <denisitch@gmail.com>
2024-05-30 10:48:53 +0300

    add unit test for controller

--- customer-app/src/test/java/com/denisitch/customer/controller/ProductControllerTest.java
+++ customer-app/src/test/java/com/denisitch/customer/controller/ProductControllerTest.java
@@ -3,6 +3,7 @@ package com.denisitch.customer.controller;
 import com.denisitch.customer.client.FavouriteProductsClient;
 import com.denisitch.customer.client.ProductReviewsClient;
 import com.denisitch.customer.client.ProductsClient;
+import com.denisitch.customer.entity.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,10 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.ConcurrentModel;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductControllerTest {
@@ -29,6 +33,39 @@ class ProductControllerTest {
 
     @InjectMocks
     ProductController controller;
+
+    @Test
+    void loadProduct_ProductExist_ReturnsNotEmptyMono() {
+        // given
+        var product = new Product(1, "Товар № 1", "Описание товара № 1");
+        doReturn(Mono.just(product)).when(this.productsClient).findProduct(1);
+        // when
+        StepVerifier.create(this.controller.loadProduct(1))
+        // then
+                .expectNext(product)
+                .expectComplete()
+                .verify();
+
+        verify(this.productsClient).findProduct(1);
+        verifyNoMoreInteractions(this.productsClient);
+        verifyNoInteractions(this.favouriteProductsClient, this.productReviewsClient);
+    }
+
+    @Test
+    void loadProduct_ProductDoesNotExist_ReturnsMonoWithNoSuchElementException() {
+        // given
+        doReturn(Mono.empty()).when(this.productsClient).findProduct(1);
+        // when
+        StepVerifier.create(this.controller.loadProduct(1))
+        // then
+                .expectErrorMatches(exception -> exception instanceof NoSuchElementException e &&
+                        e.getMessage().equals("customer.products.error.not_found"))
+                .verify();
+
+        verify(this.productsClient).findProduct(1);
+        verifyNoMoreInteractions(this.productsClient);
+        verifyNoInteractions(this.favouriteProductsClient, this.productReviewsClient);
+    }
 
     @Test
     @DisplayName("Exception NoSuchElementException will be translated to page errors/404")
